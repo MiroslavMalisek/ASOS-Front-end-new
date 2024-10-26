ae3917bb943478d5c00eb7d65d479b8af8d40fac
MiroslavMalisek <xmalisek@stuba.sk>
2024-10-26 03:05:52 +0200

    feat: Implementation of About us page

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,4 +1,6 @@
 import {Helmet} from "react-helmet-async";
+import {Card} from "react-bootstrap";
+import '../styles/AboutUs.css'
 
 export function AboutUs() {
     return (
@@ -6,7 +8,15 @@ export function AboutUs() {
             <Helmet>
                 <title>O nás</title>
             </Helmet>
-            <h1 className="text-center text-uppercase my-4">About Us</h1>
+            <Card className="about-us-card">
+                <h3 className="mb-3">Kto sme?</h3>
+                <p>Táto webová aplikácia ASOS E-shop je fiktívny e-shop, ktorý bol vytvorený v rámci predmetu
+                Architektúra softvérových systémov (ASOS) v ZS 2. ročníku ING na FEI STU. </p>
+                <p>Frontendová časť aplikácie bola vytvorená vo frameworku React a z členov tímu na vývoji podieľali
+                Bc. Miroslav Malíšek a Bc. Peter Kopecký.</p>
+                <p>Backendová časť aplikácie bola vytvorená vo frameworku Laravel a z členov tímu na vývoji podieľali
+                Bc. Michal Vrbovský, Bc. Adela Radičová a Bc. Richard Körösi.</p>
+            </Card>
         </>
 
     );
