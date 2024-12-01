f4644e42eabb645df95417664f868aa97fa18787
peter <xkopecky@stuba.sk>
2024-12-01 11:27:28 +0100

    feat: settings for Jest fw

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,12 @@
-export default {
-    preset: "ts-jest",
-    testEnvironment: "jest-environment-jsdom",
-    moduleNameMapper: {
-      "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-    },
-    setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
-  };
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+};
+
+export default config;
